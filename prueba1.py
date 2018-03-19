230c238c39e35aba802e0435e63f5518963df6ca
Tasigur <jcasanova11199@gmail.com>
2018-03-19 16:00:10 +0100

    Subiendo a github

---
+++ prueba1.py
@@ -0,0 +1,10 @@
+print("Mi primer programa en pycharm")
+print("Practiquemos con un bucle")
+
+for i in range(5):
+    print("Linea: {}".format(i))
+    print("  Probando....")
+nx=0
+for a in "abcxabcx":
+    nx+=1
+print("Hay",nx,"letrase")
